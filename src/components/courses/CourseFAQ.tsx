9d00b0bf106985fa26eaff40c92bb8d953027439
Darshan Dhabale <darshan@darshandev.tech>
2023-02-22 18:55:46 +0530

    Fix. course Faq animation

--- src/components/courses/CourseFAQ.tsx
+++ src/components/courses/CourseFAQ.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FAQ } from '@/services/course/types';
 import { uuid } from 'uuidv4';
 import styl from '@/styles/courseFAQ.module.scss';
 
 const CourseFAQ: React.FC<CourseFAQProps> = ({ courseName, faqList }) => {
+	const activeCard = useRef(0);
+
+	const handleMouseEnter = (idx) => {
+		activeCard.current = idx;
+		const cards = document.querySelectorAll('.courseFaqCard');
+		cards[idx].classList.add(styl.activeCard);
+		if ([1, 2].includes(activeCard.current)) {
+			cards[0].classList.remove(styl.activeCard);
+		}
+	};
+
+	const handleMouseLeave = (idx) => {
+		const cards = document.querySelectorAll('.courseFaqCard');
+		cards[idx].classList.remove(styl.activeCard);
+		if ([1, 2].includes(activeCard.current)) {
+			activeCard.current = 0;
+			cards[0].classList.add(styl.activeCard);
+		}
+	};
+
+	const getCardClass = (idx) => {
+		if (activeCard.current === idx) {
+			return styl.activeCard
+		};
+		return ''
+	};
 
 	return (
 		<div className="mx-5 md:mx-auto xl:w-76">
@@ -18,10 +44,9 @@ const CourseFAQ: React.FC<CourseFAQProps> = ({ courseName, faqList }) => {
 				{faqList?.length > 0 && faqList.map((faq, idx) => (
 					<div
 						key={uuid()}
-						className={styl.faqcard}
-						style={{
-							transition: 'width 0.5s'
-						}}
+						className={`courseFaqCard ${styl.faqcard} ${getCardClass(idx)}`}
+						onMouseEnter={() => handleMouseEnter(idx)}
+						onMouseLeave={() => handleMouseLeave(idx)}
 					>
 						<div className={styl.question}>
 							<p className={styl.questionNumber}>Q{idx + 1}</p>
