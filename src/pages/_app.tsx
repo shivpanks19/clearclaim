7655129644de5d3e6a0b30afc23a1b27e5dbef7a
Darshan Dhabale <darshan@darshandev.tech>
2023-02-27 15:13:08 +0530

    Fix. gtm script tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -50,11 +50,11 @@ export default function App({ Component, pageProps }: AppProps) {
     const gaScript = document.getElementById('ga');
     const fbScript = document.getElementById('fb');
 
-    gtmScript.innerHTML = `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-N7SB2Z3');</script>    
+    })(window,document,'script','dataLayer','GTM-N7SB2Z3');    
   `;
 
     gaScript.innerHTML = `window.dataLayer = window.dataLayer || [];
