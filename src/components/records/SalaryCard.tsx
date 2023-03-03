3d6a7e289be1ca497da4b9503b78695d61671640
Darshan Dhabale <darshan@darshandev.tech>
2023-03-03 15:59:17 +0530

    records SalaryCard text change

--- src/components/records/SalaryCard.tsx
+++ src/components/records/SalaryCard.tsx
@@ -6,7 +6,7 @@ const SalaryCard: React.FC<SalaryCardProps> = ({ position, numberOfSalaries, low
 		<div className='card shadow rounded flex flex-col my-3 md:my-8 p-5 w-72'>
 			<p className="text-sm md:text-lg text-primary font-semibold">{position}</p>
 			<p className="text-xs md:text-base text-aphonic mb-3">{numberOfSalaries} salaries</p>
-			<p className="text-sm md:text-base font-medium text-primary">Average Annual Salary Offered</p>
+			<p className="text-sm md:text-base font-medium text-primary">Average LPA</p>
 			<p className="text-xl md:text-3xl font-bold text-tertiary">₹ {avg}</p>
 			<SalaryBars
 				low={low}
