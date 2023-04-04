d0191fb2286b5b85571f1335b5dc9ca182d64bf9
Darshan Dhabale <darshan.dhabale@hexanovate.com>
2023-04-05 02:57:42 +0530

    Vimeo facade

--- src/components/index/DemoVideo.tsx
+++ src/components/index/DemoVideo.tsx
@@ -10,7 +10,14 @@ const DemoVideo: React.FC<DemoVideoProps> = ({ url, headline, subHeadline }) => 
 				className='mb-4'
 			/>
 			<div className="video-container relative xl:w-76 mx-auto px-5 mb-12 flex justify-center">
-				<div className='w-full rounded-3xl overflow-hidden' style={{ padding: '56.25% 0 0 0', position: 'relative' }}><iframe data-src={url ?? 'https://player.vimeo.com/video/813124028?h=bf8271f639&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position:'absolute',top:0,left:0,width:'100%',height:'100%'}} title="4k SOCIAL MEDIA"></iframe></div>
+				<div className='w-full rounded-3xl overflow-hidden'>
+					{/* <iframe data-src={url ?? 'https://player.vimeo.com/video/813124028?h=bf8271f639&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'} allow="autoplay; fullscreen; picture-in-picture" allowFullScreen style={{position:'absolute',top:0,left:0,width:'100%',height:'100%'}} title="4k SOCIAL MEDIA"></iframe> */}
+					{/* @ts-ignore */}
+					<lite-vimeo videoid="357274789" style={{backgroundImage: "url('https://i.vimeocdn.com/video/810965406.webp?mw=1600&mh=900&q=70')"}}>
+						<div className="ltv-playbtn"></div>
+						{/* @ts-ignore */}
+					</lite-vimeo>
+				</div>
 			</div>
 		</>
 	)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
 					<script id='fb'></script>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+					<script async src="http://unpkg.com/lite-vimeo-embed"></script>
 					<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
