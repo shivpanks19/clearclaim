e0c0acedf9ae90a3076d3df821c5804abc762846
Darshan Dhabale <darshan.dhabale@hexanovate.com>
2023-04-04 20:41:37 +0530

    Defer video 2s

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,7 +54,7 @@ export default function App({ Component, pageProps }: AppProps) {
           vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
         }
       }
-    }), 100)
+    }), 2000)
 
     setTimeout(attachScripts, 20000)
   }, [])
