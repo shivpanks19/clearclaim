24fbff630a272cb2e3057c9ee078a2d1e4fc00cf
Darshan Dhabale <dhabaledarshan@gmail.com>
2023-04-14 08:22:36 +0530

    Removed old scripts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,10 +19,10 @@ export default class MyDocument extends Document {
 					<script async type="module" src="https://cdn.jsdelivr.net/npm/@slightlyoff/lite-vimeo@0.1.1/lite-vimeo.js"></script>
 				</Head>
 				<body>
-					<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7SB2Z3"
+					<noscript><iframe src=""
 						height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
 					<noscript><img height="1" width="1" style={{ display: 'none' }}
-						src="https://www.facebook.com/tr?id=889335038854708&ev=PageView&noscript=1"
+						src=""
 					/></noscript>
 					<Main />
 					<NextScript />
